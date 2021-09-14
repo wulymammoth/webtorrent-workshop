ec11d86aacc6f200add82be8b459497f96001828
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2021-09-14 11:24:08 -0400

    UPDATE - conform to examples in the materials

--- echo/client.js
+++ echo/client.js
@@ -1,7 +1,11 @@
 /* For the client you can also use the net module to connect to the server.
 Just like with the connection, you can use process.stdin.on('data', ...) to
 read from stdin and process.stdout.write(data) to write to stdout. */
-const net = require("net");
-const PORT = 8124;
-const socket = net.connect(PORT, "localhost");
-process.stdin.on("data", (data) => process.stdout.write(`[client] server response: ${data}\n`));
+const net = require("net"),
+  PORT = 8124,
+  HOST = 'localhost';
+const socket = net.connect(PORT, HOST);
+
+process.stdin.on("data", (data) => socket.write(data));
+
+socket.on('data', (data) => process.stdout.write(data));
--- echo/server.js
+++ echo/server.js
@@ -2,23 +2,11 @@
 the createServer function and you'll be on your way. Once you have a socket you
 can use the .on('data' function(data) {...}) method to read data from it and
 the .write(data) method to write to it. */
-const net = require("net");
 const PORT = 8124;
-const server = net.createServer((c) => {
-  console.log("[server] client connected\n");
-  c.on("end", () => console.log("[server] client disconnected\n"));
-  c.pipe(c);
+
+const net = require('net');
+const server = net.createServer(socket => {
+  console.log('new connection')
+  socket.on('data', (data) => socket.write(data));
 });
-server.on("error", (err) => {
-  throw err;
-});
-server.listen(PORT, () => {
-  console.log(`[server] TCP server listening on port ${PORT}\n`);
-});
-server.on("connection", (socket) => {
-  socket.on("data", (chunk) => {
-    // echo the request data sent to the server
-    socket.write(chunk.toString());
-  });
-  socket.on("end", () => console.log("[server] client connection closed\n"));
-});
+server.listen(PORT);
