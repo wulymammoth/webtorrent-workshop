1cce1a2c0bd8c4ed422ea2987e9c1d50316e8dc2
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2021-09-14 11:56:58 -0400

    ADD - track active and disconnected sockets using stream-set

--- echo/client.js
+++ echo/client.js
@@ -1,11 +1,9 @@
 /* For the client you can also use the net module to connect to the server.
 Just like with the connection, you can use process.stdin.on('data', ...) to
 read from stdin and process.stdout.write(data) to write to stdout. */
-const net = require("net"),
-  PORT = 8124,
-  HOST = 'localhost';
-const socket = net.connect(PORT, HOST);
+const PORT   = 8124;
+const HOST   = 'localhost';
+const net    = require("net");
+const socket = net.connect(10000, HOST).on('data', (data) => process.stdout.write(data));
 
 process.stdin.on("data", (data) => socket.write(data));
-
-socket.on('data', (data) => process.stdout.write(data));
--- echo/server.js
+++ echo/server.js
@@ -1,12 +1,28 @@
-/* You can use the net module to create TCP servers in node. Check the docs for
-the createServer function and you'll be on your way. Once you have a socket you
-can use the .on('data' function(data) {...}) method to read data from it and
-the .write(data) method to write to it. */
-const PORT = 8124;
+/* 1. You can use the net module to create TCP servers in node. Check the docs for
+ * the createServer function and you'll be on your way. Once you have a socket you
+ * can use the .on('data' function(data) {...}) method to read data from it and
+ * the .write(data) method to write to it.
+ */
 
-const net = require('net');
-const server = net.createServer(socket => {
-  console.log('new connection')
-  socket.on('data', (data) => socket.write(data));
-});
-server.listen(PORT);
+/* 2. maintain multiple connections to the server
+ *   - also track disconnections
+ *
+ *   Luckily there is a module on npm called stream-set. It keeps track of a list of streams and remove streams from the list as they become closed. TCP sockets in node, are also streams, so you can use this module to keep track of the connections to your server.
+
+ * Now, all you need to do is write the message received from one client to the reminder of clients.
+ */
+const PORT = 10000;
+const net = require('net'), streamSet = require('stream-set'), logger = console;
+const activeSockets = streamSet();
+
+const server =
+  net.createServer(socket => {
+    logger.log('new connection')
+    activeSockets.add(socket);
+    socket
+      .on('data', (data) => socket.write(data))
+      .on('close', () => logger.log(`[socket:close] sockets: ${activeSockets.size}`));
+  }).listen(PORT, () => {
+    const socket = net.connect(PORT);
+    socket.on('connect', () => socket.destroy());
+  });
